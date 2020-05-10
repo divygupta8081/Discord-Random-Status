c295fe7ea7384816fbd5310316dca0bbfcdb3dcd
CaptainCooldev <52374169+CaptainCooldev@users.noreply.github.com>
2020-05-11 01:57:45 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext import commands
+# from itertools import cycle
+import asyncio
+import platform
+import time
+import colorsys
+import random
+prefix = "+"
+bot = commands.Bot(command_prefix=prefix)
+bot.remove_command('help')
+BOT_OWNER_ROLE="owner" #change which role you need!
+
+
+
+@bot.event
+async def on_ready():
+    print('Online')
+    print(bot.user.name)
+    print("Everything's all ready to go~")
+    while True:
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with Trivia!"))
+    	await asyncio.sleep(5)
+    	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with Created by Cool"))
+    	await asyncio.sleep(5)
+	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with My server"))
+    	await asyncio.sleep(5)
+    	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with All Users"))
+    	await asyncio.sleep(5)
+    	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with Happy moment"))
+    	await asyncio.sleep(5)
+    	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with Love"))
+    	await asyncio.sleep(5)
+    	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="With Animals"))
+    	await asyncio.sleep(5)
+    	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with Kings"))
+    	await asyncio.sleep(5)
+	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with your Love😍!"))
+    	await asyncio.sleep(5)
+	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with All cmds!"))
+    	await asyncio.sleep(5)
+	
+    	await bot.change_presence(activity=discord.Activity(type=2,name="with req.owner role"))
+    	await asyncio.sleep(5)
+	
+    	await bot.change_presence(activity=discord.Activity(type=1,name="with  members!"))
+    	await asyncio.sleep(5)
+    	
+    	await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f'''{len(bot.guilds)} servers'''))
+    	await asyncio.sleep(5)
+
+@bot.command(name="ping")
+async def ping(ctx):
+    '''
+    This text will be shown in the help command
+    '''
+
+    # Get the latency of the bot
+    latency = bot.latency  # Included in the Discord.py library
+    # Send it to the user
+    await ctx.send(latency)
+    
+bot.run("your_self_token",bot=False)
